1246d2117bd74f27d29e7881f8607603375fc759
Mariusz <mariusz.lepkowski23@gmail.com>
2025-10-17 14:58:55 +0200

    feat(admin): register Album model in Django admin panel
    Panel enhanced with search, filters and list display.

--- backend/albums/admin.py
+++ backend/albums/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Album
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ("id", "artist", "title", "year", "genre")
+    list_display_links = ("id", "title")
+    search_fields = ("artist", "title", "genre")
+    list_filter = ("year", "genre")
+    ordering = ("year", "artist")
