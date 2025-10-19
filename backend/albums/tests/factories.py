b9196b6528abd8a729f288c61c366ecac3ba31cf
Mariusz <mariusz.lepkowski23@gmail.com>
2025-10-19 13:30:39 +0200

    feat(tests): improve AlbumFactory by generating numeric year value

--- backend/albums/tests/factories.py
+++ backend/albums/tests/factories.py
@@ -1,5 +1,8 @@
 import factory
 from albums.models import Album
+from faker import Faker
+
+fake = Faker()
 
 
 class AlbumFactory(factory.django.DjangoModelFactory):
@@ -8,5 +11,5 @@ class AlbumFactory(factory.django.DjangoModelFactory):
 
     artist = factory.Faker("name")
     title = factory.Faker("sentence", nb_words=3)
-    year = factory.Faker("year")
+    year = factory.LazyFunction(lambda: int(fake.year()))
     genre = factory.Faker("word")
